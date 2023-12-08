e4265e7742158b0c6d6608140e20058a0f9875f2
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-07 20:15:45 -0500

    Se agrega algunos estilos iniciales para probar las dependencias instaladas.

--- Cotizador_Criptomonedas/criptos-react/src/App.jsx
+++ Cotizador_Criptomonedas/criptos-react/src/App.jsx
@@ -6,6 +6,11 @@ const Container = styled.div`
   max-width: 900px;
   margin: 0 auto;
   width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column.gap: 2rem;
+  }
 `
 
 const Image = styled.img`
@@ -17,6 +22,20 @@ const Image = styled.img`
 const Heading = styled.h1`
   font-family: 'Lato', sans-serif;
   color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `
 
 function App() {
@@ -27,9 +46,11 @@ function App() {
         src={imageCrypto}
         alt="Imagen criptomonedas"
       />      
-      <Heading>
-        Desde App
-      </Heading>
+      <div>
+        <Heading>
+          Cotiza Criptomonedas al Instante
+        </Heading>
+      </div>
     </Container>
   )
 }
