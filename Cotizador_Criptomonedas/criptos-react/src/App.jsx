99810882a2997484b58f9e880732900fc1740953
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-07 20:05:34 -0500

    Se agrega algunos estilos iniciales para probar las dependencias instaladas.

--- Cotizador_Criptomonedas/criptos-react/src/App.jsx
+++ Cotizador_Criptomonedas/criptos-react/src/App.jsx
@@ -1,11 +1,36 @@
 //import { useState } from 'react'
+import styled from "@emotion/styled";
+import imageCrypto from './img/imagen-criptos.png'
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+`
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+`
 
 function App() {
 
   return (
-    <h1>
-      Desde App
-    </h1>
+    <Container>
+      <Image
+        src={imageCrypto}
+        alt="Imagen criptomonedas"
+      />      
+      <Heading>
+        Desde App
+      </Heading>
+    </Container>
   )
 }
 
